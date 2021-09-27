0fe00a7c21bf8084e14f149461dcc688a271eba4
Anthony Kazyaka <ask.anthony@hotmail.com>
2021-09-27 00:05:11 -0400

    Add initial opening conditions test file

---
+++ Tests/Quaranta.Tests/OpeningConditions/OpeningConditionTests.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace Quaranta.Tests
+{
+    public class OpeningConditionTests
+    {
+        
+        public void IsHighPair()
+        {
+
+        }
+
+        public void IsNotHighPair()
+        {
+
+        }
+
+
+    }
+}
